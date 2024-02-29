b50382cf4a56f6b139061432a13e37cdecda8d87
sdussanov <safarkhan.dussanov@gmail.com>
2024-02-29 22:53:33 +0600

    deleted base pages and added navbar

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import './assets/main.css'
+//import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import "bootstrap/dist/js/bootstrap.js"
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <Navbar />
   </main>
 </template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
