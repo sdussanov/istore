c4df1f3b85147c12ceeea5f4b49af2211164001b
Safarkhan Dussanov <sdussanov@Safarkhans-MacBook-Pro.local>
2024-03-14 19:09:35 +0500

    added cart view

---
+++ src/components/Cart.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container mt-4">
+        <h2>Корзина</h2>
+        <div v-if="cart.length === 0" class="alert alert-info" role="alert">
+            Корзина пуста
+        </div>
+        <div v-else>
+            <div v-for="(item, index) in cart" :key="index" class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img :src="item.image" class="img-fluid rounded-start" alt="Product Image">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ item.name }}</h5>
+                            <p class="card-text">{{ item.description }}</p>
+                            <p class="card-text"><strong>Цена: </strong>{{ item.price }}</p>
+                            <button @click="removeFromCart(index)" class="btn btn-danger">Удалить из корзины</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-end">
+                <button @click="clearCart" class="btn btn-secondary">Очистить корзину</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cart: []
+        };
+    },
+    mounted() {
+        this.loadCart();
+    },
+    methods: {
+        loadCart() {
+            const cartData = localStorage.getItem('cart');
+            if (cartData) {
+                this.cart = JSON.parse(cartData);
+            }
+        },
+        removeFromCart(index) {
+            this.cart.splice(index, 1);
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+        },
+        clearCart() {
+            this.cart = [];
+            localStorage.removeItem('cart');
+        }
+    }
+};
+</script>
+
+<style></style>
---
+++ src/views/CartView.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        <Navbar />
+        <Cart />
+        <Footer />
+    </main>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import Cart from '../components/Cart.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+    components: {
+        Navbar,
+        Cart,
+        Footer
+    }
+}
+</script>
+
+<style></style>
